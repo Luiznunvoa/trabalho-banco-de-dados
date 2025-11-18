f0f12baa08bb2a98c962a76861ddd689415e172e
Luiznunvoa <luizgpinheiro0905@gmail.com>
2025-11-18 12:06:58 -0300

    atualização

--- consultas.sql
+++ consultas.sql
@@ -1,21 +1,115 @@
--- 1. Identificar quais são os canais patrocinados e os valores de patrocínio pagos por empresa.
---Dar a opção de filtrar os resultados por empresa como um parâmetro opcional na forma de uma 
--- stored procedure.
+SET SEARCH_PATH TO core;
 
+/* 1. Identificar quais são os canais patrocinados e os valores de patrocínio pagos por empresa.
+   SE UMA EMPRESA FEZ MAIS DE UM PATROCINIO PARA UM CANAL, PRECISA AGRUPAR?
+   */
+SELECT
+  c.nome nome_canal,
+  p.valor valor_patrocinio,
+  e.nome nome_empresa
+FROM empresa e
+JOIN patrocinio p ON p.nro_empresa = e.nro
+JOIN canal c ON p.id_canal = c.id
+ORDER BY c.nome ASC;
 
-CREATE OR REPLACE FUNCTION VALORESPOREMPRESA(id_empresa int DEFAULT null)
-RETURNS TABLE (
-  id int,
-  nome_canal varchar(255),
-  nro_plataforma int,
-  nome_empresa varchar(255),
-  valor_pago float
-)
-AS $$
-	select c.id, c.nome as nome_canal, c.nro_plataforma, e.nome as nome_empresa, sum(p.valor) as valor_pago
-	from Canal c join Patrocinio p on c.id = p.id_canal
-		join Empresa e on p.nro_empresa = e.nro
-	where id_empresa is null or e.nro = id_empresa
-	group by c.id, c.nome, c.nro_plataforma, e.nome
-	
-$$ LANGUAGE sql;
+/*2. Descobrir de quantos canais cada usuário é membro e a soma do valor desembolsado por
+usuário por mês.
+  // DEVE CONSIDERAR USUARIOS SEM INSCRIÇÃO?
+  */
+SELECT
+  u.nick AS nome,
+  COUNT(nc.id_canal) AS qnt_canais_membro,
+  SUM(nc.valor) AS valor_gasto_mes
+FROM
+  usuario AS u
+INNER JOIN inscricao AS i
+  ON u.id = i.id_membro
+INNER JOIN nivelcanal AS nc
+  ON i.id_nivel = nc.id
+GROUP BY u.nick
+ORDER BY
+  valor_gasto_mes DESC,
+  nome ASC;
+
+/* 3. Listar e ordenar os canais que já receberam doações e a soma dos valores recebidos em
+doação.
+   SE É VALOR RECEBIDO ENTÃO O STATUS_PAGAMENTO TEM QUE SER CONCLUIDO? */
+SELECT
+  d.id_canal,
+  d.nome,
+  d.qtd_doacoes,
+  d.total_doacao
+FROM
+  vw_faturamento_doacao AS d
+ORDER BY total_doacao DESC;
+
+/* 4. Listar a soma das doações geradas pelos comentários que foram lidos por vídeo.   */
+
+-- Vale a pena adicionar a coluna "coment_on" na view "vw_faturamento_doacao" ?
+SELECT
+  v.id_canal,
+  v.titulo,
+  COUNT(d.id_comentario) AS qtd_doacoes_lidas,
+  SUM(d.valor) AS total_doacao
+FROM
+  doacao AS d
+INNER JOIN comentario AS com
+  ON d.id_comentario = com.num_seq
+INNER JOIN video AS v
+  ON com.id_video = v.id
+WHERE com.coment_on = true AND d.status_pagamento = 'CONCLUIDO'
+GROUP BY v.id
+ORDER BY total_doacao DESC;
+
+/* 5. Listar e ordenar os k canais que mais recebem patrocínio e os valores recebidos.
+   usar o k = 5 por enquanto*/
+
+SELECT
+  nome,
+  qtd_patrocinios,
+  total_patrocinio
+FROM vw_faturamento_patrocinio
+ORDER BY qtd_patrocinios DESC -- Ordena pela QUANTIDADE, como na sua query
+LIMIT 5; /* usei esse comando invés de Limit para evitar que empates fiquei fora da lista */
+
+/* 6. Listar e ordenar os k canais que mais recebem aportes de membros e os valores recebidos.
+   k=5 */
+SELECT
+  nome,
+  qtd_inscricoes,
+  total_inscricao
+FROM vw_faturamento_inscricao
+ORDER BY total_inscricao DESC
+LIMIT 5;
+
+/* 7. Listar e ordenar os k canais que mais receberam doações considerando todos os vídeos.
+  k = 5  */
+
+SELECT
+  nome,
+  qtd_doacoes,
+  total_doacao
+FROM
+  vw_faturamento_doacao
+ORDER BY qtd_doacoes DESC
+LIMIT 5;
+
+/* 8. Listar os k canais que mais faturam (Patrocínio + Membros + Doações). k=5 */
+    /* uso de left join para pegar os casos que nao tem */
+SELECT
+  c.nome,
+  COALESCE(pat.total_patrocinio, 0) AS f_patrocinio,
+  COALESCE(ins.total_inscricao, 0) AS f_inscricao,
+  COALESCE(doa.total_doacao, 0) AS f_doacao,
+  (
+    COALESCE(pat.total_patrocinio, 0)
+    + COALESCE(ins.total_inscricao, 0)
+    + COALESCE(doa.total_doacao, 0)
+  ) AS faturamento_total
+FROM
+  canal c
+LEFT JOIN vw_faturamento_patrocinio pat ON c.id = pat.id_canal
+LEFT JOIN vw_faturamento_inscricao ins ON c.id = ins.id_canal
+LEFT JOIN vw_faturamento_doacao doa ON c.id = doa.id_canal
+ORDER BY faturamento_total DESC
+LIMIT 5;
--- init.sql
+++ init.sql
@@ -103,11 +103,15 @@ CREATE TABLE Canal (
   ON DELETE CASCADE ON UPDATE CASCADE
 );
 
+-- Um canal pode ser patrocinado pela mesma empresa 2 vezes
+-- 
 CREATE TABLE Patrocinio (
   id INT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
   nro_empresa INT NOT NULL,
   id_canal INT NOT NULL,
   valor DECIMAL(10, 2),
+  -- dataInicio TIMESTAMP NOT NULL,
+  -- dataFim TIMESTAMP,
 
   UNIQUE (nro_empresa, id_canal),
 
@@ -339,4 +343,52 @@ $$ LANGUAGE sql;
 -- $$ LANGUAGE sql;
 
 -- -- Views -- --- 
+
+-- Faz sentido ser view materializada?
+-- Se sim, faz sentido implementar um TRIGGER?
+CREATE VIEW vw_faturamento_doacao AS
+SELECT
+  c.id AS id_canal,
+  c.nome,
+  COUNT(d.id_comentario) AS qtd_doacoes,
+  SUM(d.valor) AS total_doacao
+FROM
+  canal AS c
+INNER JOIN video AS v
+  ON c.id = v.id_canal
+INNER JOIN comentario AS com
+  ON v.id = com.id_video
+INNER JOIN doacao AS d
+  ON
+    com.num_seq = d.id_comentario
+    AND d.status_pagamento = 'CONCLUIDO'
+GROUP BY c.id;
+
+/* V2 TOTAL DE PATROCINIOS
+       usado na Q5 e Q8 */
+CREATE VIEW vw_faturamento_patrocinio AS
+SELECT
+  c.id AS id_canal,
+  c.nome,
+  COUNT(p.id) AS qtd_patrocinios,
+  SUM(p.valor) AS total_patrocinio
+FROM
+  canal c
+JOIN patrocinio p ON c.id = p.id_canal
+GROUP BY c.id;
+
+/* V2 TOTAL DE MEMBROS
+       usado na Q6 e Q8 */
+CREATE VIEW vw_faturamento_inscricao AS
+SELECT
+  c.id AS id_canal,
+  c.nome,
+  COUNT(i.id_membro) AS qtd_inscricoes,
+  SUM(nc.valor) AS total_inscricao
+FROM
+  canal c
+JOIN nivelcanal nc ON c.id = nc.id_canal
+JOIN inscricao i ON nc.id = i.id_nivel
+GROUP BY c.id;
+
 
